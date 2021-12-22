7ab83748a09c8652131764f43b5e847901d9db0d
kusanagtiomohito <kusanagitomohitoabcd10@gmail.com>
2021-12-22 15:39:52 +0900

    add

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('user.dashboard');
+})->middleware(['auth:users'])->name('dashboard');
+
+require __DIR__.'/auth.php';
