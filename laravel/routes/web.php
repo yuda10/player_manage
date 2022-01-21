ca0c91804adf9fbf17cc72000056d88a2d49f41d
HOUCHI <1043houchi@gmail.com>
2022-01-21 14:49:09 +0900

    before merge

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -36,3 +36,26 @@ Route::post('/player', [ App\Http\Controllers\CreateController::class, 'store'] 
 Route::get('/create/', [ App\Http\Controllers\CreateController::class, 'index' ] );
 Route::get('/edit/{id}', [ App\Http\Controllers\CreateController::class, 'edit' ] )->name('player');;
 Route::post('/edit/{id}', [ App\Http\Controllers\CreateController::class, 'update'] )->name('player');
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+// 全ユーザ
+Route::group(['middleware' => ['auth', 'can:user-higher']], function () {
+// ユーザ一覧
+Route::get('/account', 'AccountController@index')->name('account.index');
+});
+// 管理者以上
+Route::group(['middleware' => ['auth', 'can:admin-higher']], function () {
+// ユーザ登録
+Route::get('/account/regist', 'AccountController@regist')->name('account.regist');
+Route::post('/account/regist', 'AccountController@createData')->name('account.regist');
+// ユーザ編集
+Route::get('/account/edit/{user_id}', 'AccountController@edit')->name('account.edit');
+Route::post('/account/edit/{user_id}', 'AccountController@updateData')->name('account.edit');
+// ユーザ削除
+Route::post('/account/delete/{user_id}', 'AccountController@deleteData');
+});
+// システム管理者のみ
+Route::group(['middleware' => ['auth', 'can:system-only']], function () {
+});
