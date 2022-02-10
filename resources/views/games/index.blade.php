bb000b0d77ab306ed6493f39ce9040c84b8b8824
yuda10 <yudakun10@gmail.com>
2022-02-10 21:47:17 +0900

    master.blade.phpの修正（以前の変更を元に戻した）

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -2,11 +2,6 @@
 
 @section('content_header')
 <h1>試合一覧</h1>
-<!-- カレンダー機能追加のため追加 -->
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 @stop
 
 @section('content')
